6ab59da41db998433c473580df2b9af4548c8b90
jaafar-alawieh <jaafar.s.alawieh@gmail.com>
2024-03-06 14:48:38 +0100

    auto rebuild commit

--- configuration.nix
+++ configuration.nix
@@ -270,6 +270,7 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+  android-tools
 	autocutsel
   brave
   copyq
