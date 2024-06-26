2bbcf1a3904bab1d47efd51c8c907729a1436265
jaafar-alawieh <jaafar.s.alawieh@gmail.com>
2024-06-26 22:10:10 +0200

    auto rebuild commit

--- configuration.nix
+++ configuration.nix
@@ -384,6 +384,7 @@ in
         cp = "cp -i";
         mv = "mv -i";
         mkdir = "mkdir -p";
+        nixclean = "nix-env --delete-generations old && nix-store --gc && nix-channel --update && nix-env -u --always && for link in /nix/var/nix/gcroots/auto/*; do rm $(readlink $link); done && nix-collect-garbage -d";
         home = "cd ~";
         cb = "cd ..";
         cbb = "cd ../..";
