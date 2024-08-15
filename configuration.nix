2ec05c3dbbb82fe649513e05a248980bf2f19a46
jaafar-alawieh <jaafar.s.alawieh@gmail.com>
2024-08-15 13:19:06 +0100

    auto rebuild commit

--- configuration.nix
+++ configuration.nix
@@ -69,6 +69,23 @@ in
     LC_TIME = "en_GB.UTF-8";
   };
 
+  services.power-profiles-daemon.enable = false; 
+  services.tlp = { #power
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "performance";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 100;
+
+      };
+  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
