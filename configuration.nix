12399e59ed7cfb6fee9408dee7d9955a5a30b317
jaafar-alawieh <jaafar.s.alawieh@gmail.com>
2025-04-03 11:20:08 +0000

    auto rebuild commit

--- configuration.nix
+++ configuration.nix
@@ -332,12 +332,12 @@ in
     #next program
   };
   
- # programs.steam = {
- #   enable = true;
-    #remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    #dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    #localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  #};
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 
   
 
