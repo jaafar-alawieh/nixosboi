567c6a853711aff135435b4cb36026d3a1957e21
jaafar-alawieh <jaafar.s.alawieh@gmail.com>
2025-04-03 12:40:47 +0100

    auto rebuild commit

--- configuration.nix
+++ configuration.nix
@@ -1,147 +1,115 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# NixOS configuration file
 
-{ config, pkgs, lib, fetchurl, self, nix-pkgs, buildEnv, ... }:
+{ config, pkgs, lib, ... }:
 
 let
-  #NUR for spotify
+  # NUR for spotify
   nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
     inherit pkgs;
-  #end of NUR for spotify
   };
-
 in
 {
-  imports =
-    [ <home-manager/nixos>
-      # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    <home-manager/nixos>
+    # Include the results of the hardware scan
+    ./hardware-configuration.nix
+  ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  #one day ill have functioning hibernate
-  #boot.resumeDevice = "/var/lib/swapfile";
-  #boot.kernelParams = [
-  #  "resume=/var/lib/swapfile"
-  #  "resume_offset=49250304"
-  #];
-
-  #update
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.allowReboot = false;
-  system.autoUpgrade.channel = "https://channels.nixos.org/nixos-24.11";
-
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  system.copySystemConfiguration = true;
-
-  # Set your time zone.
-  #time.timeZone = "Asia/Beirut";
-  #time.timeZone = "Europe/Paris";
-  # Set Default Language
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_AU.UTF-8";
-    LC_IDENTIFICATION = "en_AU.UTF-8";
-    LC_MEASUREMENT = "en_AU.UTF-8";
-    LC_MONETARY = "en_AU.UTF-8";
-    LC_NAME = "en_AU.UTF-8";
-    LC_NUMERIC = "en_AU.UTF-8";
-    LC_PAPER = "en_AU.UTF-8";
-    LC_TELEPHONE = "en_AU.UTF-8";
-    LC_TIME = "en_AU.UTF-8"; 
+  #
+  # SYSTEM CONFIGURATION
+  #
+  
+  # Basic system setup
+  system = {
+    stateVersion = "23.11"; # Do not change unless you know what you're doing
+    copySystemConfiguration = true;
+    autoUpgrade = {
+      enable = true;
+      allowReboot = false;
+      channel = "https://channels.nixos.org/nixos-24.11";
+    };
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # Bootloader configuration
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # Networking
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+  };
+  
+  # Internationalization
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_AU.UTF-8";
+      LC_IDENTIFICATION = "en_AU.UTF-8";
+      LC_MEASUREMENT = "en_AU.UTF-8";
+      LC_MONETARY = "en_AU.UTF-8";
+      LC_NAME = "en_AU.UTF-8";
+      LC_NUMERIC = "en_AU.UTF-8";
+      LC_PAPER = "en_AU.UTF-8";
+      LC_TELEPHONE = "en_AU.UTF-8";
+      LC_TIME = "en_AU.UTF-8"; 
+    };
+  };
 
-  # Configure keymap in X11
+  # Time zone settings
+  time.timeZone = "Europe/London";
+  # Alternate timezones (uncomment as needed)
+  # time.timeZone = "Europe/Paris";
+  # time.timeZone = "Asia/Beirut";
+  
+  #
+  # DESKTOP ENVIRONMENT
+  #
+  
+  # Enable the X11 windowing system with GNOME
   services.xserver = {
-    xkb.layout = "us";
-    xkb.variant = "";
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
+  };
+  
+  # XDG Portal
+  xdg.portal = {
+    enable = true;
+    # Uncomment if needed
+    # extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
-  # #hyprland xdg portal #not sure if i need this
-  xdg.portal.enable = true;
-  # xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-
-  # Enable CUPS to print documents.
+  # 
+  # HARDWARE SUPPORT
+  #
+  
+  # Printing
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
+  # Audio with PipeWire
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # Uncomment to enable JACK support
+    # jack.enable = true;
   };
-
-
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.happylime = {
-    isNormalUser = true;
-    description = "happylime";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-      #thunderbird
-    ];
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
   
-  #flatpak support
-  services.flatpak.enable = true;
-
-  ##general overrides
-
-  nixpkgs.config.packageOverrides = pkgs: rec {
-  };
-  #end of overrides
-
-
-
-  #create directories
-  systemd.tmpfiles.rules = [
-    "d /etc/nixos/conffiles 0755 root root"
-    "d /etc/nixos/conffiles/backups 0755 root root"
-    "d /etc/nixos/conffiles/scripts 0755 root root"
-  ];
-  #end of create directories
-  
-  ####only for jaafar's backlight https://www.reddit.com/r/NixOS/comments/16on943/comment/k1m3f6g/?context=3
+  # Fix for keyboard backlight - remove if not needed
   systemd.services.disable-keyboard-backlight = {
     enable = true;
     description = "Disable keyboard backlight on resume";
@@ -151,216 +119,218 @@ in
     script = ''
       /run/current-system/sw/bin/echo 0 > "/sys/devices/pci0000:00/0000:00:14.3/PNP0C09:00/VPC2004:00/leds/platform::kbd_backlight/brightness"
     '';
-    wantedBy = [
-      "sleep.target"
-      "hibernate.target"
-      "hybrid-sleep.target"
-    ];
-    after = [
-      "sleep.target"###
-      "hibernate.target"
-      "hybrid-sleep.target"
-    ];
-  };
-  #####only for jaafar's backlight - DELETE FOR OTHER COMPUTERS
-
-  
-  #GNOME DCONF SETTINGS ##Use `dconf watch /` to track stateful changes you are doing, then set them here.
-  #comment out dconf.nix line when done with it so it doesnt keep undoing new adjustments
-  #when you need to update it:
-  ##  dconf dump / | dconf2nix > dconf.nix
-  ##  sudo cp dconf.nix /etc/nixos/dconf.nix 
-  #programs.steam = {
-  #enable = true;
-  #remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  #dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  #};
-  
-  home-manager.users."happylime" = {
-    home.stateVersion="24.05";
-    programs.bash.enable = true;
-    #imports = [ ./dconf.nix ];
-    home.file."touchegg".target = "/config/touchegg/touchegg.conf";
-    home.file."touchegg".source = "/etc/nixos/conffiles/touchegg.conf";  # or .text if I just want to include everything in a nix config file and import it
+    wantedBy = [ "sleep.target" "hibernate.target" "hybrid-sleep.target" ];
+    after = [ "sleep.target" "hibernate.target" "hybrid-sleep.target" ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  ardour
-  autocutsel
-  bitwarden-desktop
-	brave
-  caligula
-  copyq
-	dconf2nix
-  firefox
-  ffmpeg_6-full
-	fsearch
-  gedit
-	gh #github cli tool
-	gimp-with-plugins
-	git
-	glaxnimate #kdenlive dependency why tf aint it predownloaded
-	gnome-frog
-  gnome-tweaks
-  dconf-editor
-  gnome-terminal
-  gnupg
-  gnupg1
-	gradience
-  gzdoom
-  home-manager
-	imagemagick #CLI converter
-	keepassxc
-  killall
-	libreoffice
-	libsForQt5.kdenlive #kdenlive
-	livecaptions
-	lmms
-  lutris
-	mediainfo #kdenlive dependency why tf aint it predownloaded
-	megacmd
-  mpv
-	mullvad-vpn
-  nano
-	neofetch
-	neovim
-	nodePackages_latest.pnpm #package for webdev experiment 
-	obs-studio
-	ocrmypdf #pdf tool
-  pass
-  #pass-wayland
-  pomodoro-gtk
-	poppler_utils #anotherpdf toolfml
-  popsicle
-	powertop
-  protonup-qt
-	python311
-	#qbittorrent
-  #sops-nix
-	#teams
-  qtpass
-	tenacity
-	tldr
-	ungoogled-chromium
-	unzip
-  vim 
-	vlc
-	vscode-fhs
-	webex
-  wget
-  wineWowPackages.stable
-  winetricks
-  wmctrl
-  ydotool
-	zip
-  zoom-us
-	#NUR
-  #gnome extensions 
-	gnomeExtensions.battery-indicator-icon 
-  gnomeExtensions.blur-my-shell
-	gnomeExtensions.caffeine
-	gnomeExtensions.clipboard-indicator
-	gnomeExtensions.dash-to-dock
-  gnomeExtensions.gsconnect
-	gnomeExtensions.just-perfection
-	gnomeExtensions.media-controls
-	gnomeExtensions.power-profile-switcher
-	gnomeExtensions.reorder-workspaces
-	gnomeExtensions.tactile
-  gnomeExtensions.panel-workspace-scroll
-	gnomeExtensions.transparent-top-bar-adjustable-transparency
-	gnomeExtensions.transparent-window-moving
-	#gnomeExtensions.scroll-panel #wait for this to be compatible; more features
-  gnomeExtensions.space-bar
-  gnomeExtensions.workspace-matrix
-	#gnome themes
-	flat-remix-gnome
-	dracula-theme
-	rose-pine-gtk-theme
-	#gnome icons
-	rose-pine-icon-theme
-	tela-icon-theme	
-  #gnome cursor
-  bibata-cursors
-	#scripts
-	#(import ./conffiles/scripts/jump_script.nix { inherit pkgs; })
-  ];
-
-  #fonts
-  fonts.packages = with pkgs; [
-  corefonts
-  vistafonts
-  wine64Packages.fonts
-  ];
-
-
-  #custom aliases for bash and maybe some future program customizations
-  programs = {
-    bash = {
-      #shellInit       
-      shellAliases = {
-        l = "ls -alh";
-        ll = "ls -l";
-        ls = "ls --color=tty";
-        conv = "code ~/projects/nix/configuration.nix";
-        revuild = "sudo cp ~/projects/nix/configuration.nix /etc/nixos/configuration.nix && sudo nixos-rebuild switch && [ $? -eq 0 ] && git -C ~/projects/nix add . && git -C ~/projects/nix commit -m 'auto rebuild commit' && git -C ~/projects/nix push";
-        conf = "sudo nano -l /etc/nixos/configuration.nix";
-        rebuild = "sudo nixos-rebuild switch && [ $? -eq 0 ] && now=$(date +'%Y-%m-%d_%H-%M-%S') && sudo cp /etc/nixos/configuration.nix /etc/nixos/conffiles/backups/configuration.$now.nix";
-        nano = "nano -l";
-        nixscripts = "cd /etc/nixos/conffiles/scripts";
-        cp = "cp -i";
-        mv = "mv -i";
-        mkdir = "mkdir -p";
-        nixclean = "nix-env --delete-generations old && nix-store --gc && nix-channel --update && nix-env -u --always && for link in /nix/var/nix/gcroots/auto/*; do rm $(readlink $link); done && nix-collect-garbage -d";
-        home = "cd ~";
-        cb = "cd ..";
-        cbb = "cd ../..";
-        cbbb = "cd ../../..";
-        cbbbb = "cd ../../../.."; 
-        doc2pdf = "soffice --headless --convert-to pdf ";
-        dconfigure = "sudo nano /etc/nixos/dconf.nix";
-        dconfbackup = "dconf dump / | dconf2nix > dconf.nix ; sudo cp dconf.nix /etc/nixos/dconf.nix; sudo cp dconf.nix ~/projects/nix/dconf.nix";
-        dconfbackuphalf = "dconf dump / | dconf2nix > dconf.nix ";
-        # Add your aliases here
-        };	  
-      promptInit = ''
-	      PS1='\[\033[48;2;237;30;121;38;2;255;255;255m\] \$ \[\033[48;2;0;135;175;38;2;237;30;121m\]\[\033[48;2;0;135;175;38;2;255;255;255m\] \u@\[\033[38;5;226m\]\h \[\033[48;2;83;85;85;38;2;0;135;175m\]\[\033[48;2;83;85;85;38;2;255;255;255m\] \w \[\033[49;38;2;83;85;85m\]\[\033[00m\] '
-  	  '' ;
-    };
-    #next program
-  };
+  #
+  # APPLICATIONS AND SERVICES
+  #
   
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
+  # Flatpak support
+  services.flatpak.enable = true;
+
+  # Steam
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+  
+  # Create necessary directories
+  systemd.tmpfiles.rules = [
+    "d /etc/nixos/conffiles 0755 root root"
+    "d /etc/nixos/conffiles/backups 0755 root root"
+    "d /etc/nixos/conffiles/scripts 0755 root root"
+  ];
+  
+  # Fonts
+  fonts.packages = with pkgs; [
+    corefonts
+    vistafonts
+    wine64Packages.fonts
+  ];
+  
+  # Package overrides (if needed)
+  nixpkgs.config.packageOverrides = pkgs: rec {
+    # Add package overrides here
+  };
+  
+  # User account
+  users.users.happylime = {
+    isNormalUser = true;
+    description = "happylime";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+    ];
   };
 
-  
+  # Home Manager configuration
+  home-manager.users.happylime = {
+    home.stateVersion = "24.05";
+    programs.bash.enable = true;
+    # Uncomment to import dconf settings
+    # imports = [ ./dconf.nix ];
+    home.file = {
+      "touchegg" = {
+        target = "/config/touchegg/touchegg.conf";
+        source = "/etc/nixos/conffiles/touchegg.conf";
+      };
+    };
+  };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # Shell customization  
+  programs.bash = {
+    shellAliases = {
+      # File management
+      l = "ls -alh";
+      ll = "ls -l";
+      ls = "ls --color=tty";
+      cp = "cp -i";
+      mv = "mv -i";
+      mkdir = "mkdir -p";
+      
+      # Navigation
+      home = "cd ~";
+      cb = "cd ..";
+      cbb = "cd ../..";
+      cbbb = "cd ../../..";
+      cbbbb = "cd ../../../..";
+      nixscripts = "cd /etc/nixos/conffiles/scripts";
+      
+      # NixOS management
+      conv = "code ~/projects/nix/configuration.nix";
+      revuild = "sudo cp ~/projects/nix/configuration.nix /etc/nixos/configuration.nix && sudo nixos-rebuild switch && [ $? -eq 0 ] && git -C ~/projects/nix add . && git -C ~/projects/nix commit -m 'auto rebuild commit' && git -C ~/projects/nix push";
+      conf = "sudo nano -l /etc/nixos/configuration.nix";
+      rebuild = "sudo nixos-rebuild switch && [ $? -eq 0 ] && now=$(date +'%Y-%m-%d_%H-%M-%S') && sudo cp /etc/nixos/configuration.nix /etc/nixos/conffiles/backups/configuration.$now.nix";
+      nixclean = "nix-env --delete-generations old && nix-store --gc && nix-channel --update && nix-env -u --always && for link in /nix/var/nix/gcroots/auto/*; do rm $(readlink $link); done && nix-collect-garbage -d";
+      
+      # dconf management
+      dconfigure = "sudo nano /etc/nixos/dconf.nix";
+      dconfbackup = "dconf dump / | dconf2nix > dconf.nix ; sudo cp dconf.nix /etc/nixos/dconf.nix; sudo cp dconf.nix ~/projects/nix/dconf.nix";
+      dconfbackuphalf = "dconf dump / | dconf2nix > dconf.nix";
+      
+      # Applications
+      nano = "nano -l";
+      doc2pdf = "soffice --headless --convert-to pdf";
+    };
+    
+    promptInit = ''
+      PS1='\[\033[48;2;237;30;121;38;2;255;255;255m\] \$ \[\033[48;2;0;135;175;38;2;237;30;121m\]\[\033[48;2;0;135;175;38;2;255;255;255m\] \u@\[\033[38;5;226m\]\h \[\033[48;2;83;85;85;38;2;0;135;175m\]\[\033[48;2;83;85;85;38;2;255;255;255m\] \w \[\033[49;38;2;83;85;85m\]\[\033[00m\] '
+    '';
+  };
 
-  # List services that you want to enable:
- # services.mullvad-vpn = {
-#	enable = true;
-#  };
-
-#  networking.iproute2.enable = true;
-#  networking.firewall.allowedTCPPorts = [ 80 443 1401 1716 1717 1718 1719 1720 1721 1722 1723 1724 1725 1726 1727 1728 1729 1730 1731 1732 1733 1734 1735 1736 1737 1738 1739 1740 1741 1742 1743 1744 1745 1746 1747 1748 1749 1750 1751 1752 1753 1754 1755 1756 1757 1758 1759 1760 1761 1762 1763 1764 ];
-#  networking.firewall.allowedUDPPorts = [ 53 1194 1195 1196 1197 1300 1301 1302 1303 1400 1401 1716 1717 1718 1719 1720 1721 1722 1723 1724 1725 1726 1727 1728 1729 1730 1731 1732 1733 1734 1735 1736 1737 1738 1739 1740 1741 1742 1743 1744 1745 1746 1747 1748 1749 1750 1751 1752 1753 1754 1755 1756 1757 1758 1759 1760 1761 1762 1763 1764 ];
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-  
-  
-  system.stateVersion = "23.11"; # Did you read the comment?
-
+  # System packages
+  environment.systemPackages = with pkgs; [
+    # Development tools
+    git
+    gh
+    nodejs
+    nodePackages_latest.pnpm
+    python311
+    vscode-fhs
+    
+    # System utilities
+    autocutsel
+    dconf2nix
+    home-manager
+    killall
+    nano
+    neofetch
+    neovim
+    powertop
+    tldr
+    vim
+    wget
+    wmctrl
+    ydotool
+    zip
+    unzip
+    
+    # Media tools
+    ardour
+    ffmpeg_6-full
+    gimp-with-plugins
+    glaxnimate
+    imagemagick
+    kdenlive
+    lmms
+    mediainfo
+    mpv
+    obs-studio
+    tenacity
+    vlc
+    
+    # Internet
+    brave
+    firefox
+    mullvad-vpn
+    ungoogled-chromium
+    zoom-us
+    webex
+    
+    # Security
+    bitwarden-desktop
+    gnupg
+    gnupg1
+    
+    # Gaming and Wine
+    lutris
+    protonup-qt
+    wineWowPackages.stable
+    winetricks
+    
+    # PDF and office
+    libreoffice
+    ocrmypdf
+    poppler_utils
+    
+    # GNOME utilities and themes
+    caligula
+    copyq
+    dconf-editor
+    gedit
+    gnome-frog
+    gnome-terminal
+    gnome-tweaks
+    gradience
+    livecaptions
+    pomodoro-gtk
+    
+    # File management
+    fsearch
+    megacmd
+    popsicle
+    
+    # GNOME extensions
+    gnomeExtensions.battery-indicator-icon 
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.caffeine
+    gnomeExtensions.clipboard-indicator
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.gsconnect
+    gnomeExtensions.just-perfection
+    gnomeExtensions.media-controls
+    gnomeExtensions.panel-workspace-scroll
+    gnomeExtensions.power-profile-switcher
+    gnomeExtensions.reorder-workspaces
+    gnomeExtensions.space-bar
+    gnomeExtensions.tactile
+    gnomeExtensions.transparent-top-bar-adjustable-transparency
+    gnomeExtensions.transparent-window-moving
+    gnomeExtensions.workspace-matrix
+    
+    # GNOME themes and icons
+    bibata-cursors
+    dracula-theme
+    flat-remix-gnome
+    rose-pine-gtk-theme
+    rose-pine-icon-theme
+    tela-icon-theme
+  ];
 }
