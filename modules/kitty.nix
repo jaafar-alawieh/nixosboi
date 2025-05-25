d904d335d365645c397cfbbf0fe51bfb9baeb653
jaafar-alawieh <jaafar.s.alawieh@gmail.com>
2025-05-26 00:36:06 +0100

    auto rebuild commit

---
+++ modules/kitty.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, lib, ... }:
+
+{
+  programs.kitty = {
+    enable = true;
+
+    # these lines get appended verbatim to ~/.config/kitty/kitty.conf
+    extraConfig = ''
+      ## ─── FONTS ───────────────────────────────
+      font_family      OverpassM Nerd Font Mono
+      font_size        14.0
+      enable_ligatures yes
+
+      ## ─── COLORS & UI ─────────────────────────
+      background       #1e1e2e
+      foreground       #cdd6f4
+      cursor           #f5c2e7
+
+      ## ─── BEHAVIOR & KEYBINDINGS ─────────────
+      hide_tab_bar_when_only_one_tab yes
+      map ctrl+shift+t new_tab
+      map ctrl+shift+w close_window
+    '';
+  };
+}
