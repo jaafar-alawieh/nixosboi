b248627870635b8616fc3e70b632689a081355d7
jaafar-alawieh <jaafar.s.alawieh@gmail.com>
2025-05-26 10:32:36 +0100

    auto rebuild commit

--- home/happylime/ghostty.nix
+++ home/happylime/ghostty.nix
@@ -3,10 +3,13 @@
 {
   home.packages = [ pkgs.ghostty ];
 
-  xdg.configFile."ghostty/config".text = ''
-    theme = UltraDark
-    foreground = #00FF00
-    font-family = OverpassM Nerd Font Mono
-    font-size = 15
-  '';
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      theme = "UltraDark";
+      foreground = "#00FF00";
+      font-family = "OverpassM Nerd Font Mono";
+      font-size = 15;
+    };
+  };
 }
