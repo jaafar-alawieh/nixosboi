c4b4a4a1c0e1c7342b8de7aa329086ba1d0fef68
jaafar-alawieh <jaafar.s.alawieh@gmail.com>
2025-05-26 12:33:16 +0100

    lol2

--- modules/system_bashrc.nix
+++ modules/system_bashrc.nix
@@ -22,8 +22,8 @@
       
       # NixOS management
       conv = "cursor ~/projects/nix/";
-      flakeupdate = "cd ~/projects/nix && sudo nix flake update nixpkgs";
-      rebuild = "cd ~/projects/nix && git pull && sudo nixos-rebuild switch --flake .#nixos && [ $? -eq 0 ] && git add flake.lock && git commit -m 'Auto-update flake & rebuild' && git push";
+      updateflake = "cd ~/projects/nix && sudo nix flake update nixpkgs";
+      rebuild = "cd ~/projects/nix && sudo nixos-rebuild switch --flake .#nixos && [ $? -eq 0 ] && git add . && git commit -m 'Auto-update flake & rebuild' && git push";
       conf = "cd ~/projects/nix && ls";
       nixclean = "sudo nix-env --delete-generations old && sudo nix-store --gc && for link in /nix/var/nix/gcroots/auto/*; do sudo rm $(readlink $link); done && sudo nix-collect-garbage -d";
 
