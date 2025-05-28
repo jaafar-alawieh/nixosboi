077cef2234fec71cb6185a6b1775f6a6ce5ccffb
jaafar-alawieh <jaafar.s.alawieh@gmail.com>
2025-05-28 11:57:21 +0100

    machiatto

--- home/happylime/environment.nix
+++ home/happylime/environment.nix
@@ -28,9 +28,10 @@
     NIXOS_XDG_OPEN_USE_PORTAL = "1";
 
     # XDG base dirs
-    XDG_CACHE_HOME         = "${HOME}/.cache";
-    XDG_CONFIG_HOME        = "${HOME}/.config";
-    XDG_DATA_HOME          = "${HOME}/.local/share";
-    # XDG_BIN_HOME         = "${HOME}/.local/bin";
+    XDG_CACHE_HOME = "\${HOME}/.cache";
+    XDG_CONFIG_HOME = "\${HOME}/.config";
+    #XDG_BIN_HOME = "\${HOME}/.local/bin";
+    XDG_DATA_HOME = "\${HOME}/.local/share";
+
   };
 }
