4c32c39631980767337386543a11ce1731f0c6dd
jaafar-alawieh <jaafar.s.alawieh@gmail.com>
2025-05-28 15:54:40 +0100

    auto rebuild commit

--- home/happylime/cursor_code.nix
+++ home/happylime/cursor_code.nix
@@ -6,29 +6,33 @@ in {
   programs.vscode = {
     enable = true;
     package = cursorPackage;
-    userSettings = {
-      # Your Cursor settings.json entries
-      "window.zoomLevel"         = 2;
-      "workbench.colorTheme"     = "Sweet Dracula";
-      "update.mode"              = "none";
-      "window.menuBarVisibility" = "visible";
-      "window.commandCenter"     = true;
+
+    profiles.default = {
+      userSettings = {
+        # Your Cursor settings.json entries
+        "window.zoomLevel"         = 2;
+        "workbench.colorTheme"     = "Sweet Dracula";
+        "update.mode"              = "none";
+        "window.menuBarVisibility" = "visible";
+        "window.commandCenter"     = true;
+      };
+
+      extensions = with pkgs.vscode-extensions; [
+        bbenoist.nix
+        bierner.markdown-preview-github-styles
+        donjayamanne.githistory
+        dracula-theme.theme-dracula
+        esbenp.prettier-vscode
+        jnoortheen.nix-ide
+        mhutchie.git-graph
+        ms-azuretools.vscode-docker
+        ms-python.python
+        ms-python.vscode-pylance
+        ms-vscode.cpptools
+        ritwickdey.liveserver
+        saoudrizwan.claude-dev
+        yzhang.markdown-all-in-one
+      ];
     };
-    extensions = with pkgs.vscode-extensions; [
-      bbenoist.nix
-      bierner.markdown-preview-github-styles
-      donjayamanne.githistory
-      dracula-theme.theme-dracula
-      esbenp.prettier-vscode
-      jnoortheen.nix-ide
-      mhutchie.git-graph
-      ms-azuretools.vscode-docker
-      ms-python.python
-      ms-python.vscode-pylance
-      ms-vscode.cpptools
-      ritwickdey.liveserver
-      saoudrizwan.claude-dev   # now available via nixpkgs
-      yzhang.markdown-all-in-one
-    ];
   };
 }
