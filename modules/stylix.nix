1bd084f8d88db78122cf4b9dba9f5719e11637bf
jaafar-alawieh <jaafar.s.alawieh@gmail.com>
2025-05-28 13:10:46 +0100

    flakey

---
+++ modules/stylix.nix
@@ -0,0 +1,41 @@
+# ~/projects/nix/modules/stylix.nix
+{ pkgs, ... }:
+{
+  stylix.enable = true;
+
+  # Color scheme https://tinted-theming.github.io/tinted-gallery/
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+
+  # Cursor: Bibata Modern Classic
+  stylix.cursor.package = pkgs.bibata-cursors;
+  stylix.cursor.name    = "Bibata-Modern-Classic";
+  stylix.cursor.size    = 24;
+
+  # Fonts configuration
+  stylix.fonts = {
+    # Terminal font: Overpass Mono Nerd Font Mono at size 15
+    monospace = {
+      package = pkgs.nerd-fonts.overpass;
+      name    = "OverpassM Nerd Font Mono";
+    };
+    # UI & documents font: Inter Variable at size 14
+    sansSerif = {
+      package = pkgs.inter;
+      name    = "Inter Variable";
+    };
+    serif = {
+      package = pkgs.inter;
+      name    = "Inter Variable";
+    };
+  };
+
+  # Font sizes
+  stylix.fonts.sizes = {
+    applications = 13;  # UI elements
+    desktop      = 10;
+    popups       = 10;
+    terminal     = 15;
+  };
+
+  stylix.image = ../assets/backgrounds/background1;
+}
