40133475fa0481d5762adf727489249104986124
jaafar-alawieh <jaafar.s.alawieh@gmail.com>
2025-05-29 02:47:11 +0100

    auto rebuild commit

--- home/happylime/cursor_code.nix
+++ home/happylime/cursor_code.nix
@@ -10,20 +10,32 @@ in {
     profiles.default = {
       userSettings = {
         # Your Cursor settings.json entries
+
+        # General Appearance
         "window.zoomLevel"         = 2;
         "workbench.colorTheme"     = "Sweet Dracula";
         "update.mode"              = "none";
         "window.menuBarVisibility" = "visible";
         "window.commandCenter"     = true;
+
+        # Paths
         "git.path"                 = "/run/current-system/sw/bin/git";
         "python.defaultInterpreterPath" = "/run/current-system/sw/bin/python3";
+        "terminal.integrated.defaultProfile.linux" = "bash";
+        "terminal.integrated.profiles.linux" = {
+          "bash" = {
+            "path" = "/run/current-system/sw/bin/bash";
+          };
+        };
+
+        # Modified tab colors
         "workbench.editor.highlightModifiedTabs" = true;
-         "workbench.colorCustomizations" = {
-           "tab.activeModifiedBorder" = "#ff79c6";
-           "tab.inactiveModifiedBorder" = "#ff79c6";
-        #   "tab.activeDirtyBorder" = "#50fa7b";
-        #   "tab.inactiveDirtyBorder" = "#50fa7b";
-         };
+        "workbench.colorCustomizations" = {
+          "tab.activeModifiedBorder" = "#ff79c6";
+          "tab.inactiveModifiedBorder" = "#ff79c6";
+          "tab.activeDirtyBorder" = "#50fa7b";
+          "tab.inactiveDirtyBorder" = "#50fa7b";
+        };
       };
 
       extensions = with pkgs.vscode-extensions; [
